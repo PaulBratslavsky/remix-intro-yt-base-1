bc9c51def4ee659cb0a838b69d099d12b3428407
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-01-10 10:26:52 -0600

    initial commit

---
+++ app/components/custom/Navigation.tsx
@@ -0,0 +1,59 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/RpImR1PAsTR
+ */
+
+import { Link } from "@remix-run/react";
+import {
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenu,
+} from "~/components/ui/navigation-menu";
+
+export default function Navigation() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+      <div className="w-full px-4 py-6 flex items-center justify-between">
+        <div className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+          MyApp
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList className="flex gap-4">
+            <NavigationMenuLink asChild>
+              <Link
+                to="/"
+                className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+              >
+                Home
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+                to="#"
+              >
+                Features
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+                to="#"
+              >
+                Pricing
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
+                to="#"
+              >
+                Contact
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
+}
